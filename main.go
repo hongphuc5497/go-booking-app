3a294b75c608260956e988a0df6c12c78b3b83f3
Hong Phuc <hongphuc.dthp@gmail.com>
2023-03-27 21:26:13 +0700

    go-learning: data types and formatted output

--- main.go
+++ main.go
@@ -5,11 +5,29 @@ import "fmt"
 
 // A program can only have 1 "main" function, because we only have 1 entrypoint
 func main() {
-	var confName = "Go Conf"
-	const confTickets = 50
-	var remainingTickets = 50
- 	
-	fmt.Println("Welcome to", confName, "booking application")
- 	fmt.Println("We have a total of", confTickets, "tickets and", remainingTickets, "are still available")
+	// var confName = "Go Conf"
+	confName := "Go Conf" // Syntatix suger with variable, not apply to constant
+	const confTickets int = 50
+	remainingTickets := 50
+
+	// String Interpolation ---
+	// Solution 1.
+	// fmt.Println("Welcome to", confName, "booking application")
+ 	// fmt.Println("We have a total of", confTickets, "tickets and", remainingTickets, "are still available")
+ 	// fmt.Println("Get your tickers here to attend")
+
+	// Solution 2
+	fmt.Printf("confName is %T, confTickets is %T, remainingTickets is %T\n", confName, confTickets, remainingTickets)
+	fmt.Printf("Welcome to %v booking application\n", confName)
+ 	fmt.Printf("We have a total of %v tickets and %v are still available \n", confTickets, remainingTickets)
  	fmt.Println("Get your tickers here to attend")
+
+	// Data Type ---
+	var userName string
+	var userTickets int
+
+	userName = "Test"
+	userTickets = 100
+
+	fmt.Printf("User %v have booked %v tickets", userName, userTickets)
 }
